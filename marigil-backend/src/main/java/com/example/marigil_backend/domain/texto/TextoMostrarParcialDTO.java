951c79bc90e1fdd379761c7b97e6b73d74880560
Felipesdr <felipederose@gmail.com>
2024-07-28 20:21:46 -0300

    fix: adicionado um atributo de coleção de CategoriaDetalhadoDTO ao record TextoMostrarParcialDTO

--- marigil-backend/src/main/java/com/example/marigil_backend/domain/texto/TextoMostrarParcialDTO.java
+++ marigil-backend/src/main/java/com/example/marigil_backend/domain/texto/TextoMostrarParcialDTO.java
@@ -1,8 +1,20 @@
 package com.example.marigil_backend.domain.texto;
 
-public record TextoMostrarParcialDTO(Long idTexto, String titulo, String subtitulo, String imagemUrl) {
+import com.example.marigil_backend.domain.categoria.CategoriaDetalhadoDTO;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public record TextoMostrarParcialDTO(Long idTexto, String titulo, String subtitulo, String imagemUrl, Set<CategoriaDetalhadoDTO> categorias) {
 
     public TextoMostrarParcialDTO(Texto texto){
-        this(texto.getIdTexto(), texto.getTitulo(), texto.getSubTitulo(), texto.getImgUrl());
+        this(
+                texto.getIdTexto(),
+                texto.getTitulo(),
+                texto.getSubTitulo(),
+                texto.getImgUrl(),
+                texto.getCategorias().stream().map(c-> new CategoriaDetalhadoDTO(c.getIdCategoria(), c.getNome())).collect(Collectors.toSet())
+        );
     }
 }
